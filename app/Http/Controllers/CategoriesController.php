135fdc0715ac9284215edba195c14dba941bf707
isa-fares <isafares1@gmail.com>
2026-06-18 16:22:44 +0300

    UPDATE: certae categories controller and service layer and reques validition

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -2,64 +2,66 @@
 
 namespace App\Http\Controllers;
 
+use Inertia\Inertia;
 use App\Models\Category;
-use Illuminate\Http\Request;
+use App\Services\CategoryService;
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
 
-class CategoriesController extends Controller
+class CategoryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    public function __construct(
+        private CategoryService $categoryService
+    ) {}
+
     public function index()
     {
-        //
+        return Inertia::render('Categories/Index', [
+            'categories' => $this->categoryService->getAll(),
+        ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
-        //
+        return Inertia::render('Categories/Create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    public function store(StoreCategoryRequest $request)
     {
-        //
+        $this->categoryService->create(
+            $request->validated()
+        );
+
+        return redirect()
+            ->route('categories.index')
+            ->with('success', 'Category created successfully');
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Category $category)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(Category $category)
     {
-        //
+        return Inertia::render('Categories/Edit', [
+            'category' => $category,
+        ]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Category $category)
+    public function update( UpdateCategoryRequest $request, Category $category ) 
     {
-        //
+        $this->categoryService->update(
+            $category,
+            $request->validated()
+        );
+
+        return redirect()
+            ->route('categories.index')
+            ->with('success', 'Category updated successfully');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Category $category)
     {
-        //
+        $this->categoryService->delete($category);
+
+        return redirect()
+            ->back()
+            ->with('success', 'Category deleted successfully');
     }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -58,3 +58,6 @@ class Product extends Model
         return $this->belongsTo(Category::class);
     }
 }
+
+
+
